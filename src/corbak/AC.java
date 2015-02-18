cff5dbe6f3e4044c4788e473dc6e0c879c691072
Arnaud-Loisy <arnaud.loisy@gmail.com>
2015-02-18 19:32:55 +0100

    d

---
+++ src/corbak/AC.java
@@ -0,0 +1,10 @@
+package corbak;
+
+/**
+ * Interface definition : AC
+ * 
+ * @author OpenORB Compiler
+ */
+public interface AC extends ACOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity
+{
+}
