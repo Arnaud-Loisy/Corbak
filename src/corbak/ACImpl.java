63ce6c4e7e4eed8993cbfa87914963441877d1e7
Mathieu Raoux <mathieu.raoux@gmail.com>
2015-03-19 16:24:48 +0100

    Création de ACImpl

---
+++ src/corbak/ACImpl.java
@@ -0,0 +1,42 @@
+package corbak;
+
+import java.util.ArrayList;
+
+import org.omg.CORBA.Object;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+
+public class ACImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+	
+	ArrayList listAV;
+	ArrayList<Certificat> listCert;
+	org.omg.CORBA.ORB orb;
+	
+	public ACImpl (org.omg.CORBA.ORB orb)
+	{
+		listAV=new ArrayList();
+		listCert = new ArrayList<Certificat>();
+		this.orb=orb;
+		
+	}
+	
+	public Certificat generateCertificat(String PubKey, Date dateExpiration, org.omg.CORBA.Object ACemmetrice, Signature sign)
+	{
+		Certificat cert = new Certificat();
+		cert.pubClef=PubKey;
+		cert.dateExpiration=dateExpiration;
+		cert.ACemmetrice=ACemmetrice;
+		cert.sign=sign;
+		listCert.add(cert);
+		return cert;
+	}
+}
